573b2a3c0217cbcb03d14d2413ff08d309ae5a07
Luca8991 <luca.berte98@hotmail.it>
2023-11-18 20:21:40 +0100

    Initial commit.

---
+++ src/supsi_example_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { supsi_example_backend } from "../../declarations/supsi_example_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await supsi_example_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
